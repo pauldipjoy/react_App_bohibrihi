c91880bc2c79833bf9b452692c1290354251954b
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-05 18:00:49 +0600

    set React Router Package

--- src/App.js
+++ src/App.js
@@ -401,14 +401,26 @@
 
 
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainComponent from './components/MainComponent';
 
 
 
+
 const App = () => {
 
-  return <MainComponent />
+  return (
+
+<BrowserRouter>
+
+    
+    <MainComponent />
+
+
+</BrowserRouter>
+  
+)
 
 
 }
